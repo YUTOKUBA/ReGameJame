823b409dde4b6e707ca62bb3399b192b7bdaaa88
takaaki4869 <107020923+takaaki4869@users.noreply.github.com>
2022-07-23 16:25:02 +0900

    Update SceneMgr.h

--- ReGameJame/SceneMgr.h
+++ ReGameJame/SceneMgr.h
@@ -4,8 +4,8 @@ typedef enum
 	eScene_Menu, //メニュー画面
 	eScene_Game, //ゲーム画面
 	eScene_Config, //設定画面
-	eScene_None, //無
-	し
+	eScene_None, //無し
+	
 } eScene;
 void SceneMgr_Initialize();//初期化
 void SceneMgr_Finalize();//終了処理
