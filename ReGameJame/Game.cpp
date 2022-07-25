9195b1639bb150a44e0163a6a6b87e3af63ae5ed
川平花 <hana-kawahira-k21h@stu.kbc.ac.jp>
2022-07-25 13:23:13 +0900

    制限時間の追加
    制限時間が0になるかHキーを押してゲームオーバー画面に遷移します。

--- ReGameJame/Game.cpp
+++ ReGameJame/Game.cpp
@@ -2,9 +2,11 @@
 #include "SceneMgr.h"
 #include "DxLib.h"
 static int mImageHandle; //画像ハンドル格納用変数
+float g_TimeLimit; 
 //初期化
 void Game_Initialize() {
 	mImageHandle = LoadGraph("images/Scene_Game.png"); //画像のロード
+	g_TimeLimit = 60 * (TIMELIMIT + 1); //制限時間をセット
 }
 //終了処理
 void Game_Finalize() {
@@ -15,7 +17,12 @@ void Game_Update() {
 	if (CheckHitKey(KEY_INPUT_F) != 0) { //Escキーが押されていたら
 		SceneMgr_ChangeScene(eScene_Clear);//シーンをメニューに変更
 	}
-	if (CheckHitKey(KEY_INPUT_H) != 0) { //Escキーが押されていたら
+	//if (CheckHitKey(KEY_INPUT_H) != 0) { //Escキーが押されていたら
+	//	SceneMgr_ChangeScene(eScene_GameOver);//シーンをメニューに変更
+	//}
+	
+	//計測時間を過ぎたらゲームオーバー
+	if (g_TimeLimit-- <= 60 || CheckHitKey(KEY_INPUT_H) != 0) {
 		SceneMgr_ChangeScene(eScene_GameOver);//シーンをメニューに変更
 	}
 }
@@ -25,4 +32,6 @@ void Game_Draw() {
 	DrawString(20, 380, "ゲーム画面です。", GetColor(136, 136, 255));
 	DrawString(20, 400, TEXT("Fキーを押すとゲームクリア画面に行きます。"), GetColor(136,136, 255));
 	DrawString(20, 420, TEXT("Hキーを押すとゲームオーバー画面に行きます。"), GetColor(136,136, 255));
+
+	DrawFormatString(320, 50, 0x000000, "%2.2f", g_TimeLimit / 60); //制限時間の描画
 }
