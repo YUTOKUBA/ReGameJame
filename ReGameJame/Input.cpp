a19e559eb0cd24add731251e33c01376b9c40981
YUTOKUBA <yuuto-kuba-20h@stu.kbc.ac.jp>
2022-07-26 14:09:10 +0900

    Input.hを修正

--- ReGameJame/Input.cpp
+++ ReGameJame/Input.cpp
@@ -2,13 +2,19 @@
 #include"DxLib.h"
 #include"Game.h"
 
-int	g_OldKey;				// 前回の入力キー
-int	g_NowKey;				// 今回の入力キー
-int	g_KeyFlg;				// 入力キー情報
+int	g_OldKey;				
+int	g_NowKey;				
+int	g_KeyFlg;			
+
+XINPUT_STATE XNowKey;      
+XINPUT_STATE XOldKey;
 
 void InputKey(void) {
 	//入力キー取得
 	g_OldKey = g_NowKey;
 	g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	g_KeyFlg = g_NowKey & ~g_OldKey;
+
+	XOldKey = XNowKey;
+	GetJoypadXInputState(DX_INPUT_PAD1, &XNowKey);
 }
