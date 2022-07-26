0751ec8cbd3b58da6d5ede0d00ad4e851067b2f9
YUTOKUBA <yuuto-kuba-20h@stu.kbc.ac.jp>
2022-07-26 13:37:47 +0900

    Creditを追加

--- ReGameJame/Credit.cpp
+++ ReGameJame/Credit.cpp
@@ -1,25 +1,27 @@
+#include "Credit.h"
 #include "SceneMgr.h"
 #include "DxLib.h"
 #include "Input.h"
 
+
 static int mImageHandle; //画像ハンドル格納用変数
 //初期化
-void Result_Initialize() {
-	mImageHandle = LoadGraph("images/Scene_Result.png"); //画像のロード
+void Credit_Initialize() {
+	mImageHandle = LoadGraph("images/Scene_GaneCredit.png"); //画像のロード
 }
 //終了処理
-void Result_Finalize() {
+void Credit_Finalize() {
 	DeleteGraph(mImageHandle); //画像の解放
 }
 //更新
-void Result_Update() {
+void Credit_Update() {
 	if (CheckHitKey(KEY_INPUT_ESCAPE) != 0 || XNowKey.Buttons[XINPUT_BUTTON_A]) {//Escキーが押されていたらorBボタンが押されていたら
 		SceneMgr_ChangeScene(eScene_Menu);//シーンをメニューに変更
 	}
 }
 //描画
-void Result_Draw() {
+void Credit_Draw() {
 	DrawGraph(0, 0, mImageHandle, FALSE);
-	DrawString(20, 380, "リザルト画面です。", GetColor(136, 136, 255));
-	DrawString(20, 400, "SPACEキーを押すとタイトル画面に進みます。", GetColor(136, 136, 255));
+	DrawString(20, 380, "credit画面です。", GetColor(136, 136, 255));
+	DrawString(20, 400, "Bボタンを押すとMenu画面に進みます。", GetColor(136, 136, 255));
 }
