bf2c6a396f650eb2f3ee0115a9fe91b0c357b177
oumadgij <106945464+oumadgij@users.noreply.github.com>
2022-07-26 10:26:14 +0900

    コントローラーの入力キー取得処理の追加

--- ReGameJame/Input.cpp
+++ ReGameJame/Input.cpp
@@ -1,10 +1,19 @@
 #include<stdio.h>
-#include"Input.h"
 #include"DxLib.h"
+ int	g_OldKey;				// 前回の入力キー
+ int	g_NowKey;				// 今回の入力キー
+ int	g_KeyFlg;				// 入力キー情報
+
+ XINPUT_STATE XNowKey;       //コントローラー用今回の入力キー
+ XINPUT_STATE XOldKey;       //     ,,         前回の入力キー
+
 
 void InputKey(void) {
 	//入力キー取得
 	g_OldKey = g_NowKey;
 	g_NowKey = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 	g_KeyFlg = g_NowKey & ~g_OldKey;
+
+	XOldKey = XNowKey;
+	GetJoypadXInputState(DX_INPUT_PAD1, &XNowKey);
 }
--- ReGameJame/Input.h
+++ ReGameJame/Input.h
@@ -1,9 +1,13 @@
 #pragma once
+#include "DxLib.h"
 
 //変数の宣言
-int	g_OldKey;				// 前回の入力キー
-int	g_NowKey;				// 今回の入力キー
-int	g_KeyFlg;				// 入力キー情報
+extern int	g_OldKey;				// 前回の入力キー
+extern int	g_NowKey;				// 今回の入力キー
+extern int	g_KeyFlg;				// 入力キー情報
+
+extern XINPUT_STATE XNowKey;       //コントローラー用今回の入力キー
+extern XINPUT_STATE XOldKey;       //     ,,         前回の入力キー
 
 //関数の宣言
 void InputKey(void);				//パッド入力
