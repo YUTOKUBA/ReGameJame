fcff98550f4abdb068bad22bad756a506d934b66
kyuya11 <83804045+kyuya11@users.noreply.github.com>
2022-07-27 15:04:56 +0900

    答えを選択した時に一時停止するように修正

--- ReGameJame/Game.cpp
+++ ReGameJame/Game.cpp
@@ -6,7 +6,11 @@
 #include "Question.h"
 
 static int mImageHandle; //画像ハンドル格納用変数
+static int SE_Atari;
+static int SE_Hazure;
 float g_TimeLimit;
+bool g_TimeStop;
+int g_TimeStopCount;
 int a;
 int seikai;
 int sippai;
@@ -14,7 +18,11 @@ int sippai;
 //初期化
 void Game_Initialize() {
 	mImageHandle = LoadGraph("images/Scene_GameMain.png"); //画像のロード
+	SE_Atari = LoadSoundMem("BGM_SE/Seikai.mp3");	//SEのロード
+	SE_Hazure = LoadSoundMem("BGM_SE/Huseikai.mp3");	//SEのロード
 	g_TimeLimit = 60 * (TIMELIMIT); //制限時間をセット
+	g_TimeStopCount = 0;
+	g_TimeStop = false;
 	QUESTION.Init();
 	QUESTION.Question_select();
 	QUESTION.Question_input();
@@ -30,7 +38,8 @@ void Game_Finalize() {
 
 //更新
 void Game_Update() {
-	 
+
+
 	//計測時間を過ぎたらゲームオーバー
 	if (g_TimeLimit-- <= 1 || CheckHitKey(KEY_INPUT_H) != 0) {
 		TimeResult = g_TimeLimit;
@@ -43,12 +52,16 @@ void Game_Update() {
 			Ans_State = 1;	//正解
 			seikai++;
 			QUESTION.SetQCount();
+			PlaySoundMem(SE_Atari, DX_PLAYTYPE_BACK, TRUE);
+			WaitTimer(1000);
 		}
 		else
 		{
 			Ans_State = 2;	//不正解
 			sippai++;
 			QUESTION.SetQCount();
+			PlaySoundMem(SE_Hazure, DX_PLAYTYPE_BACK, TRUE);
+			WaitTimer(1000);
 		}
 	}
 	else if (g_KeyFlg && XNowKey.Buttons[XINPUT_BUTTON_B]) {
@@ -57,15 +70,20 @@ void Game_Update() {
 			Ans_State = 1;	//正解
 			seikai++;
 			QUESTION.SetQCount();
+			PlaySoundMem(SE_Atari, DX_PLAYTYPE_BACK, TRUE);
+			WaitTimer(1000);
 		}
 		else
 		{
 			Ans_State = 2;//不正解
 			sippai++;
 			QUESTION.SetQCount();
+			PlaySoundMem(SE_Hazure, DX_PLAYTYPE_BACK, TRUE);
+			WaitTimer(1000);
 		}
 	}
-	if (g_KeyFlg && XNowKey.Buttons[XINPUT_BUTTON_B]|| g_KeyFlg && XNowKey.Buttons[XINPUT_BUTTON_A]) {
+
+	if (g_KeyFlg && XNowKey.Buttons[XINPUT_BUTTON_B] || g_KeyFlg && XNowKey.Buttons[XINPUT_BUTTON_A]) {
 		a++;
 		if (a >= 10) {
 			TimeResult = g_TimeLimit;
@@ -73,6 +91,11 @@ void Game_Update() {
 		}
 	}
 
+	//}
+	//else {
+	//	if (++g_TimeStopCount > 60) {
+	//		g_TimeStop = false;
+	//	}
 }
 
 //描画
